5ee9ffc23663263c9a0780f16010a571411e65c0
SenH <sen@senhaerens.be>
2014-04-03 12:10:16 +0200

    Updated for Redmine 2.x

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2009 Pierre Paysant-Le Roux
+# Copyright (c) 2014 Pierre Paysant-Le Roux
  
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -24,27 +24,21 @@ require 'svn/client'
 
 require_dependency 'repository/subversion'
 
-require 'dispatcher'
-
-Dispatcher.to_prepare do 
+Rails.configuration.to_prepare do
   Repository::Subversion.class_eval do
-    def scm_adapter_with_libsvn
+    def self.scm_adapter_class
       Redmine::Scm::Adapters::SubversionLibsvnAdapter
     end
-
-    alias_method_chain :scm_adapter, :libsvn
   end
 end
 
 Redmine::Plugin.register :libsvn do
   name 'Libsvn'
   author 'Pierre Paysant-Le Roux'
-  description 'Use libsvn as backend to access Subversion repositories'
-  url 'http://github.com/pplr/redmine-libsvn/tree/master'
-  author_url 'http://github.com/pplr/redmine-libsvn/tree/master'
-  version '0.0.1'
-  
-  requires_redmine :version_or_higher => '0.8.0'
-
+  description 'Use libsvn as a backend to access Subversion repositories'
+  url 'https://github.com/SenH/redmine-libsvn'
+  version '0.0.2'
   settings :default => {'trust_server_cert' => '0'}, :partial => 'settings/lib_svn_settings'
+  
+  requires_redmine :version_or_higher => '2.0.0'
 end
